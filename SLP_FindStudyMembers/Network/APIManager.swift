7233bd15c3c79774a48a2a36a581724ad958e857
YongSeok Lee <paninology@gmail.com>
2022-11-15 19:13:06 +0900

    파베변경

--- SLP_FindStudyMembers/Network/APIManager.swift
+++ SLP_FindStudyMembers/Network/APIManager.swift
@@ -20,7 +20,7 @@ class APIManager {
 //        let url = EndPoint.SeSACBaseURL + "/v1/" + URLSuffix.user.rawValue
         let api = SeSACStudyAPI.signIn(
             phoneNumber: UserDefaultManager.getUserDefault(key: .phoneNumber),
-            FCMtoken: UserDefaultManager.getUserDefault(key: .phoneNumber),
+            FCMtoken: "dzjnejNDh0d0u1aLzfS547:APA91bFvQSjDVFC4-2IA0QQ08KqsEKwIoK2hFBZIfdyNLPd22PvgLD6YM_kyQgv0BIK-1zjltbbKYQTGK50Pn21bctsuEC46qo7RDkcImbzyZBe0-ffMqhFhL4DO5tbP0Ri_Wn-vRVF5",
             nick: UserDefaultManager.getUserDefault(key: .nickName),
             birth: UserDefaultManager.getUserDefault(key: .birthday),
             email: UserDefaultManager.getUserDefault(key: .userEmail),
